31abb9f8ca8562d07ba396bc18682ea92fac63c0
twonine <1185366392@qq.com>
2025-04-24 22:05:03 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/my-blog', //github仓库名称
+  trailingSlash: true, //路径结尾都添加 ‘/’
+  images:{
+    unoptimized:true,
+  }
+};
 
 export default nextConfig;
